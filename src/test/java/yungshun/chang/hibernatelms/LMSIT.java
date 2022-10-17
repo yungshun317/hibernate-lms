f5b14a4adaa3d59c5f415c4e90a1daeef9add6bc
yungshun317 <seraph317@gmail.com>
2022-10-17 16:24:33 +0800

    [CHORE] Use the Maven Failsafe Plugin for running integration tests

---
+++ src/test/java/yungshun/chang/hibernatelms/LMSIT.java
@@ -0,0 +1,17 @@
+package yungshun.chang.hibernatelms;
+
+import org.junit.Test;
+
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertTrue;
+
+public class LMSIT {
+
+    @Test
+    public void givenMySQLIsRunningThenNoException() throws UnknownHostException {
+
+        // Run `mvn clean compiler:testCompile failsafe:integration-test`
+        assertTrue(true);
+    }
+}
