28e877a25b37575d4dbb6f276412d46ace3ab6d0
yungshun317 <seraph317@gmail.com>
2022-10-17 17:06:18 +0800

    [TEST] Add `mysql-connector-java` to `pom.xml` and run the integration test

--- src/test/java/yungshun/chang/hibernatelms/LMSIT.java
+++ src/test/java/yungshun/chang/hibernatelms/LMSIT.java
@@ -2,16 +2,25 @@ package yungshun.chang.hibernatelms;
 
 import org.junit.Test;
 
-import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class LMSIT {
 
     @Test
-    public void givenMySQLIsRunningThenNoException() throws UnknownHostException {
+    public void givenMySQLIsRunningThenNoException() throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hibernate_lms";
+        String user = "yungshun";
+        String password = "lucifer317";
 
         // Run `mvn clean compiler:testCompile failsafe:integration-test`
-        assertTrue(true);
+        Connection conn = DriverManager.getConnection(jdbcUrl, user, password);
+        assert(true);
     }
 }
