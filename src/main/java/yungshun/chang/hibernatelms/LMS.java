370f187ea5a07e6ce0ace5110e0635346b17a33f
yungshun317 <seraph317@gmail.com>
2022-10-18 20:32:59 +0800

    [CHORE] Include the `create-db.sql` SQL script for creating a database and tables

--- src/main/java/yungshun/chang/hibernatelms/LMS.java
+++ src/main/java/yungshun/chang/hibernatelms/LMS.java
@@ -1,7 +1,26 @@
 package yungshun.chang.hibernatelms;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class LMS {
 
     public static void main(String[] args) {
+
+        // [1] Create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // [2] Create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+        } finally {
+            factory.close();
+        }
     }
 }
