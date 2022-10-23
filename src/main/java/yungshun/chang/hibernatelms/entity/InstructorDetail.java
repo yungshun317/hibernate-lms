8e873ad2031031e848bfbecbdc2268b7aa6c545a
yungshun317 <seraph317@gmail.com>
2022-10-24 05:32:14 +0800

    [FEAT] Create the `InstructorDetail` entity class

---
+++ src/main/java/yungshun/chang/hibernatelms/entity/InstructorDetail.java
@@ -0,0 +1,61 @@
+package yungshun.chang.hibernatelms.entity;
+
+import javax.persistence.*;
+
+// Annotate the class as an entity and map to db table
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+    // Define the fields
+    // Annotate the fields with db column names
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    // Create constructors
+    public InstructorDetail() {}
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // Generate getter/setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // Generate toString() method
+    @Override
+    public String toString() {
+        return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+    }
+}
