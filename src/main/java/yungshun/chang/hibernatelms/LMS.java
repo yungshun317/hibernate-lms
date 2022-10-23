114d4a560fbe8dbaf38b9ed1e50854484c09f722
yungshun317 <seraph317@gmail.com>
2022-10-24 06:06:23 +0800

    [FEAT] Create and associate the objects with a one-to-one mapping relationship

--- src/main/java/yungshun/chang/hibernatelms/LMS.java
+++ src/main/java/yungshun/chang/hibernatelms/LMS.java
@@ -3,6 +3,8 @@ package yungshun.chang.hibernatelms;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import yungshun.chang.hibernatelms.entity.Instructor;
+import yungshun.chang.hibernatelms.entity.InstructorDetail;
 
 public class LMS {
 
@@ -18,7 +20,26 @@ public class LMS {
         Session session = factory.getCurrentSession();
 
         try {
+            // [3] Create the objects
+            Instructor tmpInstructor = new Instructor("Joe", "Johnson", "joejohnson@gmail.com");
 
+            InstructorDetail tmpInstructorDetail = new InstructorDetail("https://www.youtube.com", "Hiking");
+
+            // [4] Associate the objects
+            tmpInstructor.setInstructorDetail(tmpInstructorDetail);
+
+            // [5] Start a transaction
+            session.beginTransaction();
+
+            // [6] Save the instructor
+            // This will also save the details object because of CascadeType.ALL
+            System.out.println("Saving instructor: " + tmpInstructor);
+            session.save(tmpInstructor);
+
+            // [7] Commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done.");
         } finally {
             factory.close();
         }
