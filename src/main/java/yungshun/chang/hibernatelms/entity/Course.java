88d306fb872051651adab3308ce4c8f0e1266a94
yungshun317 <seraph317@gmail.com>
2022-10-31 09:24:11 +0800

    [FEAT] Create the `Course` entity class

---
+++ src/main/java/yungshun/chang/hibernatelms/entity/Course.java
@@ -0,0 +1,60 @@
+package yungshun.chang.hibernatelms.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    // Define fields
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    // Annotate fields
+    @ManyToOne(cascade={CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    // Define constructors
+    public Course() {}
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    // Define getter/setter methods
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    // Define toString
+    @Override
+    public String toString() {
+        return "Course [id=" + id + ", title=" + title + "]";
+    }
+}
